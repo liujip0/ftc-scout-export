80fce9f855dac2d6d6566ac276973f93373e5add
Kennan <kennanhunter5@gmail.com>
2025-01-14 00:36:36 -0500

    make it download csv

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,29 @@
 
 import Image from "next/image";
 
+type TepRecord = {
+  noFilterRank: number;
+  data: {
+    teamNumber: number;
+    stats: {
+      opr: {
+        totalPoints: number;
+        totalPointsNp: number;
+        autoPoints: number;
+        dcPoints: number;
+      };
+    };
+  };
+};
+
+type TepRecordsResponse = {
+  data: {
+    tepRecords: {
+      data: TepRecord[];
+    };
+  };
+};
+
 const buildCSV = () => {
   const myHeaders = new Headers();
   myHeaders.append("Content-Type", "application/json");
@@ -13,7 +36,7 @@ const buildCSV = () => {
 
   while (skip + take < 6000) {
     const graphql = JSON.stringify({
-      query: `{\r\n  tepRecords(season:2024, skip:${skip}, take:${take}, region:All, ) {\r\n    data {\r\n      noFilterRank\r\n      data {\r\n        teamNumber\r\n        stats { \r\n            ... on TeamEventStats2024 {\r\n            opr { totalPoints, totalPointsNp }}\r\n        }\r\n      }\r\n    }\r\n  }\r\n}\r\n`,
+      query: `{\r\n  tepRecords(season:2024, skip:${skip}, take:${take}, region:All, ) {\r\n    data {\r\n      noFilterRank\r\n      data {\r\n        teamNumber\r\n        stats { \r\n            ... on TeamEventStats2024 {\r\n            opr { totalPoints, totalPointsNp, autoPoints, dcPoints }}\r\n        }\r\n      }\r\n    }\r\n  }\r\n}\r\n`,
       variables: {},
     });
     const requestOptions = {
@@ -26,10 +49,36 @@ const buildCSV = () => {
       fetch("https://api.ftcscout.org/graphql", requestOptions)
         .then((response) => response.text())
         .then((result) => {
-          let obj = JSON.parse(result);
+          let obj: TepRecordsResponse = JSON.parse(result);
+
+          let { data } = obj.data.tepRecords;
+
+          console.dir({ data });
+
+          return data.map(
+            ({
+              data: {
+                teamNumber,
+                stats: { opr },
+              },
+            }) =>
+              `${teamNumber}, \
+${opr.totalPoints.toFixed(2)}, \
+${opr.totalPointsNp.toFixed(2)}, \
+${opr.autoPoints.toFixed(2)}, \
+${opr.dcPoints.toFixed(2)}`
+          );
         })
     );
+
+    skip += take;
   }
+
+  Promise.all(queries_arr).then((data) => {
+    let csv = "teamnumber, opr, nopr, auto, teleop \n" + data.flat().join("\n");
+
+    downloadBlob(new Blob([csv], { type: "text/csv" }));
+  });
 };
 
 const downloadBlob = (fileBlob: Blob) => {
@@ -37,7 +86,9 @@ const downloadBlob = (fileBlob: Blob) => {
 
   element.href = URL.createObjectURL(fileBlob);
 
-  element.download = `data.csv`;
+  let now = new Date();
+
+  element.download = `ftc-export-${now.getFullYear()}-${now.getMonth()}-${now.getDate()}@${now.getHours()}:${now.getMinutes()}:${now.getSeconds()}.csv`;
 
   document.body.appendChild(element); // Required for this to work on FireFox
 
@@ -53,8 +104,6 @@ export default function Home() {
         <button
           onClick={() => {
             buildCSV();
-
-            downloadBlob(new Blob([]));
           }}
           className="text-black  bg-white p-4"
         >
